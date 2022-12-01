aaeca1eb39ad902e46e83f5be658beb59250cb0d
MagnusSandbergITHS <magnus.sandberg@iths.se>
2022-12-01 09:25:41 +0100

    Update bookstore.sql
    3 vyer gjorda

--- bookstore.sql
+++ bookstore.sql
@@ -2,6 +2,8 @@
 --USE Bokhandel;
 
 
+------------------------| TABLE: Författare |------------------------
+
 --CREATE TABLE FörfattareTbl (
 --    [ID] int NOT NULL,
 --    [Förnamn] nvarchar(100) NOT NULL,
@@ -19,7 +21,8 @@
 --(4, 'Fredrik', 'Backman', '1981-06-02'),
 --(5, 'Lucinda', 'Riley', '1965-02-16');
 
-----------------------------------------------------------------
+
+------------------------| TABLE: Böcker |------------------------
 
 --CREATE TABLE BöckerTbl (
 --    [ISBN] nchar(13) NOT NULL,
@@ -48,7 +51,8 @@
 --(9789180060547, 'Änglaträdet', 'Svenska', 216.00, '2021-10-25', 592, 5),
 --(9789189057340, 'Fjärilsrummet', 'Svenska', 218.00, '2020-04-17', 616, 5);
 
-----------------------------------------------------------------
+
+------------------------| TABLE: Butiker |------------------------
 
 --CREATE TABLE ButikerTbl (
 --    [ButiksID] int NOT NULL,
@@ -56,6 +60,7 @@
 --    [Adress] nvarchar(100) NOT NULL,
 --    [Postnummer] nchar(5) NOT NULL,
 --	[Ort] nvarchar(100) NOT NULL,
+--	CHECK (LEN([Postnummer]) = 5),
 --	PRIMARY KEY ([ButiksID])
 --);
 
@@ -66,7 +71,8 @@
 --(2, 'Akademibokhandeln', 'Älvebacken 13', 44248, 'Kungälv'),
 --(3, 'Pocket Shop', 'Drottningtorget 5', 41103, 'Göteborg');
 
-----------------------------------------------------------------
+
+------------------------| TABLE: LagerSaldo |------------------------
 
 --CREATE TABLE LagerSaldoTbl (
 --	[ButiksID] int NOT NULL,
@@ -112,7 +118,8 @@
 --(3, 9789180060547, 1),
 --(3, 9789189057340, 698);
 
-----------------------------------------------------------------
+
+------------------------| TABLE: Kunder |------------------------
 
 --CREATE TABLE KunderTbl (
 --    [KundId] int NOT NULL,
@@ -130,54 +137,94 @@
 --KunderTbl ([KundId], [Förnamn], [Efternamn], [Email], [Adress], [Postnummer], [Ort])
 --VALUES 
 --(1, 'Magnus', 'Sandberg', 'magnus.sandberg@iths.se', 'Hasselvägen 2', '44277', 'Romelanda'),
---(2, 'Jessica', 'Fletcher', 'murder.she@wrote.com', ' 698 Candlewood Lane', '12345', 'Maine'),
---(3, 'Phileas', 'Fogg', 'aroundtheworldin@eightydays.uk', '7 Savile Row', '36080', 'London');
+--(2, 'Jessica', 'Fletcher', 'murder.she@wrote.com', '698 Candlewood Lane', '12345', 'Maine'),
+--(3, 'Phileas', 'Fogg', 'aroundtheworldin@eightydays.uk', '7 Savile Row', '36080', 'London'),
+--(4, 'Peter', 'Petersson', 'bokmaleng@bok.se', 'Petergatan 5', '55433', 'Grums'),
+--(5, 'Jan', 'Jansson', 'thereaderof@books.com', ' Jansvägen 37', '45611', 'Karlskoga');
 
-----------------------------------------------------------------
+
+------------------------| TABLE: Order |------------------------
 
 --CREATE TABLE OrderTbl (
 --    [OrderId] int NOT NULL,
 --    [OrderDatum] date NOT NULL,
---	[KundEmail] nvarchar(255) unique NOT NULL,
+--	[KundEmail] nvarchar(255) NOT NULL,
 --	[ProductId] nchar(13) NOT NULL,
 --	[Antal] int NOT NULL,
 --	[Styckpris] decimal (10,2) NOT NULL,
---	[Totalpris] decimal (10,2) NOT NULL, --(KAN MAN TA STYCKPRIS * ANTAL HÄR?)
 --	CHECK (LEN([ProductID]) = 13),
---	PRIMARY KEY ([OrderId]),
---	FOREIGN KEY ([KundEmail]) REFERENCES KunderTbl([Email]),
---	FOREIGN KEY ([ProductId]) REFERENCES BöckerTbl([ISBN])
+--	CONSTRAINT PK_OrderId	PRIMARY KEY ([OrderId])
 --);
 
 --INSERT INTO 
---OrderTbl ([OrderId], [OrderDatum], [KundEmail], [ProductId], [Antal], [Styckpris], [Totalpris])
+--OrderTbl ([OrderId], [OrderDatum], [KundEmail], [ProductId], [Antal], [Styckpris])
 --VALUES 
---(1, '1984-08-30', 'murder.she@wrote.com', '9781556595851', 1, 354.00, 354.00),
---(2, '2000-01-01', 'aroundtheworldin@eightydays.uk', '9789137158679', 2, 219.00, 438.00),
---(3, '2022-03-12', 'magnus.sandberg@iths.se', '9789127161023', 27, 232.00, 6264.00);
+--(1, '1984-08-30', 'murder.she@wrote.com', '9781556595851', 1, 354.00),
+--(2, '2000-01-01', 'aroundtheworldin@eightydays.uk', '9789137158679', 2, 219.00),
+--(3, '2022-03-12', 'magnus.sandberg@iths.se', '9789127161023', 4, 232.00),
+--(4, '2022-03-12', 'bokmaleng@bok.se', '9789137155524', 3, 210.00),
+--(5, '2022-03-12', 'thereaderof@books.com', '9781556595851', 2, 354.00),
+--(6, '2022-04-07', 'murder.she@wrote.com', '9781556595851', 1, 354.00),
+--(7, '2022-04-26', 'aroundtheworldin@eightydays.uk', '9789137158679', 2, 219.00),
+--(8, '2022-05-12', 'magnus.sandberg@iths.se', '9789127161023', 4, 232.00),
+--(9, '2022-06-02', 'murder.she@wrote.com', '9781556595851', 2, 354.00);
 
 
---SKAPA VY
--- Vyn ska innehålla följande 4 kolumner (med en rad per författare):
---”Namn” – Hela namnet på författaren. 
---”Ålder” – Hur gammal författaren är. 
---”Titlar” – Hur många olika titlar vi har i 
---”Böcker” av den angivna författaren. 
---”Lagervärde” – Totala värdet (pris) för författarens böcker i samtliga butiker.
+------------------------| VIEW: TitlarPerFörfattare |------------------------
 
-CREATE VIEW vTitlarPerFörfattare AS
-SELECT 
-CONCAT([Förnamn], ' ', [Efternamn]) AS Namn,
-CONCAT(DATEDIFF(hour, [Födelsedatum], GETDATE())/8766, + ' år') AS Ålder,
-CONCAT(COUNT(DISTINCT BöckerTbl.Titel), ' st') AS AntalTitlar,
-CONCAT(SUM(LagerSaldoTbl.Antal * BöckerTbl.Pris), ' kr') AS Lagervärde
-FROM 
-FörfattareTbl
-JOIN BöckerTbl ON FörfattareTbl.ID = BöckerTbl.FörfattareID
-JOIN LagerSaldoTbl ON BöckerTbl.ISBN = LagerSaldoTbl.ISBN
-GROUP BY
-FörfattareTbl.ID, Förnamn, Efternamn, Födelsedatum;
+--CREATE VIEW vTitlarPerFörfattare AS
+--SELECT 
+--CONCAT([Förnamn], ' ', [Efternamn]) AS Namn,
+--CONCAT(DATEDIFF(hour, [Födelsedatum], GETDATE())/8766, + ' år') AS Ålder,
+--CONCAT(COUNT(DISTINCT BöckerTbl.Titel), ' st') AS AntalTitlar,
+--CONCAT(SUM(LagerSaldoTbl.Antal * BöckerTbl.Pris), ' kr') AS Lagervärde
+--FROM 
+--FörfattareTbl
+--JOIN BöckerTbl ON FörfattareTbl.ID = BöckerTbl.FörfattareID
+--JOIN LagerSaldoTbl ON BöckerTbl.ISBN = LagerSaldoTbl.ISBN
+--GROUP BY
+--FörfattareID, Förnamn, Efternamn, Födelsedatum;
 
---Ändra till namn på nycklarna
---Begin, commit
---Lägg till junction-tables (many to many)
+
+------------------------| VIEW: TopSeller |------------------------
+
+-- Vyn visar vilka deras populäraste böcker är och boken de tjänar bäst på.
+-- Detta underlättar vilka böcker de måste hålla extra koll på i lagersaldot
+-- och även vilka böcker de kan göra mer/mindre reklam för
+
+--CREATE VIEW vTopSeller AS
+--SELECT
+--[ProductId],
+--SUM([Antal]) AS AntalSålda,
+--SUM([Antal] * [Pris]) AS Beställningsvärde
+--FROM
+--OrderTbl
+--JOIN BöckerTbl ON OrderTbl.ProductId = BöckerTbl.ISBN
+--GROUP BY [ProductId], [Pris];
+
+--SELECT TOP 3 * FROM vTopSeller
+--ORDER BY Beställningsvärde DESC;
+
+------------------------| VIEW: TopCustomer |------------------------
+
+-- Vy för att se vilka kunder som beställer mest böcker och för högst ordervärde.
+-- Här delar vi in kunderna i olika kategorier för att dela ut olika rabatter eller rikta kampanjer.
+
+--CREATE VIEW vBästaKunderna AS
+--SELECT
+--[KundId],
+--COUNT(DISTINCT [OrderTbl].OrderId) AS AntalBeställningar,
+--SUM(DISTINCT [Antal] * [Styckpris]) AS TotaltSpenderat,
+--CASE
+--	WHEN SUM(DISTINCT [Antal] * [Styckpris]) > 200 AND SUM(DISTINCT [Antal] * [Styckpris]) < 699  THEN 'Brons'
+--	WHEN SUM(DISTINCT [Antal] * [Styckpris]) > 700 AND SUM(DISTINCT [Antal] * [Styckpris]) < 999 THEN 'Silver'
+--	WHEN SUM(DISTINCT [Antal] * [Styckpris]) > 1000 THEN 'Guld'
+--	ELSE 'Inga ordrar'
+--END AS KundNivå
+--FROM
+--KunderTbl
+--JOIN OrderTbl ON OrderTbl.KundEmail = KunderTbl.Email
+--GROUP BY [KundId];
+
+--SELECT TOP 1 * FROM vBästaKunderna
+--ORDER BY TotaltSpenderat DESC;
